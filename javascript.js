4a76a80e732b526ba2a049f0fbd2bad50838ef7e
Alejandro Alvarez <ala007@ucsd.edu>
2025-03-09 22:08:38 -0700

    added a dialog element to be able to add additional books to the display

--- javascript.js
+++ javascript.js
@@ -26,8 +26,49 @@ function displayBooks() {
   })
 }
 
+// Adding a function to only append the most recent book should make it run faster
+function displayMostRecentBook() {
+  bookContainer = document.querySelector(".bookContainer");
+  let currentBook = document.createElement("div");
+  book = myLibrary.at(-1);
+  for(element in book) {
+    let current = document.createElement("p");
+    current.textContent = book[element];
+    currentBook.appendChild(current);
+  }
+  currentBook.setAttribute("class", "book");
+  bookContainer.appendChild(currentBook);
+}
+
 addBookToLibrary("How to Win Friends", "Dale Carnegie", 250, "Have Read");
 addBookToLibrary("Meditations", "Marcus Aurelius", 200, "Have Read");
 
-
 displayBooks();
+
+const openButton = document.querySelector(".openButton");
+const cancelButton = document.querySelector(".cancelButton");
+const addButton = document.querySelector(".addButton");
+const dialog = document.querySelector("dialog");
+const form = document.querySelector(".form");
+
+openButton.addEventListener("click", () => {
+  dialog.showModal();
+})
+
+cancelButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const bookName = form.bookName.value;
+  const authorName = form.authorName.value;
+  const numberOfPages = form.numberOfPages.value;
+  const haveReadYet = form.haveReadYet.value;
+  addBookToLibrary(bookName, authorName, numberOfPages, haveReadYet);
+  displayMostRecentBook();
+  dialog.close();
+});
+
+
+
