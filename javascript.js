bd79b1d618e8e1f0e28b0a2ce48c5b0962e2cdfc
Alejandro Alvarez <ala007@ucsd.edu>
2025-03-16 01:36:22 -0700

    cleaned up file and removed the added books for testing

--- javascript.js
+++ javascript.js
@@ -27,34 +27,6 @@ function addBookToLibrary(title, author, pages, haveRead) {
   myLibrary.push(bookToAdd);
 }
 
-function displayBooks() {
-  const bookContainer = document.querySelector(".bookContainer");
-  myLibrary.forEach((book) => {
-    let currentBook = document.createElement("div");
-    for(element in book) {
-      if(element != "id" && element != "changeReadValue") {
-        let current = document.createElement("p");
-        current.textContent = book[element];
-        currentBook.appendChild(current)
-      }
-    }
-    // let haveReadAtrribute = document.createElement("p");
-
-    let toggleButton = document.createElement("button");
-    toggleButton.textContent = "Change read status";
-    toggleButton.setAttribute("class", "toggleButton");
-    currentBook.appendChild(toggleButton);
-    let removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
-    removeButton.setAttribute("class", "removeButton");
-    currentBook.appendChild(removeButton);
-    currentBook.setAttribute("class", "book");
-    currentBook.setAttribute("data-id", book.id);
-    bookContainer.appendChild(currentBook);
-  })
-}
-
-// Adding a function to only append the most recent book should make it run faster
 function displayMostRecentBook() {
   bookContainer = document.querySelector(".bookContainer");
   let currentBook = document.createElement("div");
@@ -108,10 +80,6 @@ function updateToggleButtons() {
 })
 }
 
-addBookToLibrary("How to Win Friends", "Dale Carnegie", 250, "Have read");
-addBookToLibrary("Meditations", "Marcus Aurelius", 200, "Have read");
-
-displayBooks();
 updateRemoveButtons();
 updateToggleButtons();
 
